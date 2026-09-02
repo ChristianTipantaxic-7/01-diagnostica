3d321998dca86496df92d683211f8e0536e5ae66
ChristianTipantaxic-7 <cristipantaxic@gmail.com>
2026-09-01 19:29:47 -0500

    feat(bloque_a): Realice el ejercicio 1

--- bloque_a/Ejercicio1.java
+++ bloque_a/Ejercicio1.java
@@ -1,8 +1,15 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        // TODO: Declara aquí tus variables y asígnales valores
+        // CHECK: Declara aquí tus variables y asígnales valores
+        int numero = 0;
+        String cadena = "hola";
+        boolean estado = false;
+        double flotante = 1.15;
 
-        // TODO: Muestra los valores por consola
-        
+        // CHECK: Muestra los valores por consola
+        System.out.println(numero);
+        System.out.println(cadena);
+        System.out.println(estado);
+        System.out.println(flotante);
     }
 }
